36e328628e7e0135cb2bbfe4e5ede16d56b0a59e
sourabhchicmic <sourabh.sharma@chicmic.co.in>
2021-11-22 15:42:51 +0530

    added dark and light mode

--- src/Component/Forms/UpperText.jsx
+++ src/Component/Forms/UpperText.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const UpperText = () => {
   const [text, setText] = useState("");
+  const [preview, setPreview] = useState(false);
 
   const onChangeHandler = (e) => {
     setText(e.target.value);
@@ -13,13 +14,19 @@ const UpperText = () => {
   };
 
   const ResetHandler = (e) => {
-   
-    
     setText("");
   };
+
+  const setPreviewHandler = () => {
+    setPreview(!preview);
+    if(!preview){
+    document.body.style.backgroundColor ='black'}else{
+        document.body.style.backgroundColor ='white'  
+    }
+  };
   return (
-    <div className="container">
-        <h1 className="text-primary my-3">Enter text for changing case !</h1>
+    <div className={`container ${preview ?"bg-dark text-white" :""}`}>
+      <h1 className="text-primary my-3">Enter text for changing case !</h1>
       <div class="mb-3">
         <textarea
           value={text}
@@ -31,9 +38,31 @@ const UpperText = () => {
         ></textarea>
       </div>
 
-      <button className="btn btn-primary me-4" onClick={UperCaseHandler}>Change to UpperCase</button> 
-      <button className="btn btn-primary ms-3" onClick={ResetHandler}>Reset</button>
-     <p className="text text-center">Number of words are <i className=" fw-bold text-primary"> {text.split(" ").length} </i> and number of characters are <i className=" fw-bold text-primary">{text.trim().length}</i></p>
+      <button className="btn btn-primary me-4" onClick={UperCaseHandler}>
+        Change to UpperCase
+      </button>
+      <button className="btn btn-primary ms-3" onClick={ResetHandler}>
+        Reset
+      </button>
+      <button disabled="true" className="btn btn-primary ms-3" onClick={setPreviewHandler}>
+        Preview
+      </button>
+
+      <button className="btn btn-primary ms-3" onClick={setPreviewHandler}>
+        {preview?"Default Mode":"DarkMode"}
+      </button>
+      <p className="text text-center">
+        Number of words are
+        <i className=" fw-bold text-primary"> {text.split(" ").length} </i> and
+        number of characters are
+        <i className=" fw-bold text-primary">{text.trim().length}</i>
+      </p>
+      
+      {/* {preview && (
+        <div className="row">
+          <div style={{textAlign:'justify'}} className="col-md-8 offset-2  border border- table-bordered opacity-75 ">{text}</div>
+        </div>
+      )} */}
     </div>
   );
 };
